3114a2f80b6d31b817c343c6b73dd809b122948b
NimrodNate <nimrodnate@Computer.home>
2018-10-21 14:44:50 +0100

    test

---
+++ halloween-arkit/README.md
@@ -0,0 +1,2 @@
+# vccp-arkit-rnd
+This repo is for testing ARkit Quick look usdz file type
---
+++ halloween-arkit/service-worker.js
@@ -0,0 +1,48 @@
+var dataCacheName = 'PWA-Date-usd';
+var cacheName = 'PWA-usd';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/img/pumpkin.jpg',
+    '/usd/halloween-pumpkin.usdz'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
