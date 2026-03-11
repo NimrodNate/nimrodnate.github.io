c48cfb944f6a2c424eb692444b18d93e04685663
Nathaniel Hulley <nathanielhulley@gmail.com>
2026-03-11 14:40:22 +0000

    Removed the older site assets

--- js/script.js
+++
@@ -1,7 +0,0 @@
-window.onload =  navSet;
-
-function navSet(){
-	//console.log("load");
-	//document.getElementById('blog').href = "http://nimrodnate.tumblr.com/";
-	//document.getElementById('devart').href = "http://browse.deviantart.com/?qh=&section=&global=1&q=nimrodnate";
-}
